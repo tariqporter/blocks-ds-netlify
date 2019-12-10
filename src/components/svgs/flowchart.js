b7ebba4afca85c3c8654d8b62a99c43f0ca63dd2
Tariq <tic084@gmail.com>
2019-12-10 15:27:02 -0500

    add svgs

---
+++ src/components/svgs/flowchart.js
@@ -0,0 +1,132 @@
+import React from "react"
+
+const Flowchart = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="844" height="993" viewBox="0 0 844 993">
+    <g fill="none" fillRule="evenodd">
+      <g fillRule="nonzero">
+        <path fill="#B5BAB9" d="M95 617.5V570h1v47.5h3.5v1h-8v-1H95zM635.5 922H608v-1h27.5v-3.5h1v8h-1V922zM457.5 832.5v-42.158H402c-9.113 0-16.5-7.387-16.5-16.5V724h1v49.842c0 8.56 6.94 15.5 15.5 15.5h56.5V832.5h4l-4.5 9-4.5-9h4z"/>
+        <path stroke="#B5BAB9" d="M387 713.667V774c0 8.837-7.163 16-16 16H264c-8.837 0-16-7.163-16-16V559"/>
+        <path fill="#B5BAB9" d="M449.5 618.5v-31.455c0-8.56-6.94-15.5-15.5-15.5h-32c-9.113 0-16.5-7.388-16.5-16.5V500h1v55.045c0 8.56 6.94 15.5 15.5 15.5h32c9.113 0 16.5 7.387 16.5 16.5V618.5h4l-4.5 9-4.5-9h4z"/>
+        <path fill="#B5BAB9" d="M215.5 502.19h15.859c9.113 0 16.5 7.388 16.5 16.5V556c0 8.56 6.94 15.5 15.5 15.5H371c8.56 0 15.5-6.94 15.5-15.5v-83h1v83c0 9.113-7.387 16.5-16.5 16.5H263.359c-9.113 0-16.5-7.387-16.5-16.5v-37.31c0-8.56-6.94-15.5-15.5-15.5H215.5v4l-9-4.5 9-4.5v4zM391 190.5v-144h1v144h4l-4.5 9-4.5-9h4z"/>
+        <path fill="#B5BAB9" d="M693.5 191.5V134c0-8.56-6.94-15.5-15.5-15.5H407c-8.56 0-15.5-6.94-15.5-15.5V74h1v29c0 8.008 6.492 14.5 14.5 14.5h271c9.113 0 16.5 7.387 16.5 16.5v57.5h4l-4.5 9-4.5-9h4zM693.5 414.5V337c0-8.56-6.94-15.5-15.5-15.5H407c-8.56 0-15.5-6.94-15.5-15.5v-29h1v29c0 8.008 6.492 14.5 14.5 14.5h271c9.113 0 16.5 7.387 16.5 16.5v77.5h4l-4.5 9-4.5-9h4zM390.5 414.5V337c0-8.56-6.94-15.5-15.5-15.5H114c-8.56 0-15.5-6.94-15.5-15.5v-29h1v29c0 8.008 6.492 14.5 14.5 14.5h261c9.113 0 16.5 7.387 16.5 16.5v77.5h4l-4.5 9-4.5-9h4z"/>
+        <path fill="#B5BAB9" d="M391 420.5v-144h1v144h4l-4.5 9-4.5-9h4z"/>
+      </g>
+      <g transform="translate(644 848)">
+        <rect width="200" height="145" fill="#D9E1E0" fillRule="nonzero" rx="5"/>
+        <text fill="#2B3836" fontSize="14">
+          <tspan x="12" y="19">In our biweekly Blocks </tspan> <tspan x="12" y="37">Talks, the core Blocks </tspan> <tspan x="12" y="55">team will go through the </tspan> <tspan x="12" y="73">&quot;Ideas/Bugs&quot; column, </tspan> <tspan x="12" y="91">prioritize stories and move </tspan> <tspan x="12" y="109">them to the &quot;Design: </tspan> <tspan x="12" y="127">Discovery&quot; column.</tspan>
+        </text>
+      </g>
+      <g transform="translate(400 848)">
+        <rect width="200" height="145" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Add a ticket to the &quot;Ideas/</tspan> <tspan x="12" y="37">Bugs&quot; column in the </tspan> <tspan x="12" y="55">Blocks clubhouse with </tspan> <tspan x="12" y="73">your proposed name and </tspan> <tspan x="12" y="91">usage examples from 2 or </tspan> <tspan x="12" y="109">more products. </tspan>
+        </text>
+      </g>
+      <g transform="translate(400 771)">
+          <rect width="118" height="37" fill="#FDB100" fillRule="nonzero" rx="5"/>
+          <text fill="#FFF" fontSize="14">
+              <tspan x="12" y="19">Major release*</tspan>
+          </text>
+      </g>
+      <g transform="translate(260 771)">
+        <rect width="118" height="37" fill="#39CF94" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Minor release†</tspan>
+        </text>
+      </g>
+      <g transform="translate(291 638)">
+        <rect width="200" height="93" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Will adding your </tspan> <tspan x="12" y="37">component trigger a </tspan> <tspan x="12" y="55">major* release or a minor† </tspan> <tspan x="12" y="73">release?</tspan>
+        </text>
+      </g>
+      <g transform="translate(0 626)">
+        <rect width="200" height="145" fill="#D9E1E0" fillRule="nonzero" rx="5"/>
+        <text fill="#2B3836" fontSize="14">
+          <tspan x="12" y="19">In our biweekly Blocks </tspan> <tspan x="12" y="37">Design Talks, the design </tspan> <tspan x="12" y="55">team will go through the </tspan> <tspan x="12" y="73">&quot;Design:Discovery&quot; </tspan> <tspan x="12" y="91">column, prioritize stories </tspan> <tspan x="12" y="109">and assign designer </tspan> <tspan x="12" y="127">owners and reviewers.</tspan>
+        </text>
+      </g>
+      <g transform="translate(400 542)">
+        <rect width="113" height="56" fill="#FDB100" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Pricey like a </tspan> <tspan x="12" y="37">modal</tspan>
+        </text>
+      </g>
+      <g transform="translate(265 542)">
+        <rect width="113" height="56" fill="#39CF94" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Cheap as a </tspan> <tspan x="12" y="37">button</tspan>
+        </text>
+      </g>
+      <g transform="translate(593 437)">
+        <rect width="200" height="181" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Sorry, Blocks only </tspan> <tspan x="12" y="37">includes patterns shared </tspan> <tspan x="12" y="55">across multiple products </tspan> <tspan x="12" y="73">to prevent the system </tspan> <tspan x="12" y="91">from exploding. Create an </tspan> <tspan x="12" y="109">Icebox ticket in the Blocks </tspan> <tspan x="12" y="127">clubhouse board so we </tspan> <tspan x="12" y="145">have a record for the </tspan> <tspan x="12" y="163">future.</tspan>
+        </text>
+      </g>
+      <g transform="translate(291 437)">
+        <rect width="200" height="72" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">How expensive will your </tspan> <tspan x="12" y="37">component be to make </tspan> <tspan x="12" y="55">and maintain?</tspan>
+        </text>
+      </g>
+      <g transform="translate(0 437)">
+        <rect width="200" height="127" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Great! Add a ticket to the </tspan> <tspan x="12" y="37">&quot;Design:Discovery&quot; </tspan> <tspan x="12" y="55">column in the Blocks </tspan> <tspan x="12" y="73">clubhouse with your </tspan> <tspan x="12" y="91">proposed name and usage </tspan> <tspan x="12" y="109">examples from 3 or more </tspan> <tspan x="12" y="127">products. </tspan>
+        </text>
+      </g>
+      <g transform="translate(669 132)">
+        <rect width="48" height="34" fill="#EB5534" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">No</tspan>
+        </text>
+      </g>
+      <g transform="translate(318 343)">
+        <rect width="156" height="37" fill="#FDB100" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Yes, 1 other product</tspan>
+        </text>
+      </g>
+      <g transform="translate(38 343)">
+        <rect width="125" height="56" fill="#39CF94" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Yes, 2 or more </tspan> <tspan x="12" y="37">other products</tspan>
+        </text>
+      </g>
+      <g transform="translate(593 207)">
+        <rect width="200" height="56" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Back to the drawing </tspan> <tspan x="12" y="37">board!</tspan>
+        </text>
+      </g>
+      <g transform="translate(291 207)">
+        <rect width="200" height="93" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Is your component </tspan> <tspan x="12" y="37">relevant to any other </tspan> <tspan x="12" y="55">products? If so, how </tspan> <tspan x="12" y="73">many?</tspan>
+        </text>
+      </g>
+      <g transform="translate(669 343)">
+        <rect width="48" height="34" fill="#EB5534" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">No</tspan>
+        </text>
+      </g>
+      <g transform="translate(367 132)">
+        <rect width="48" height="34" fill="#39CF94" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Yes</tspan>
+        </text>
+      </g>
+      <g transform="translate(291)">
+        <rect width="200" height="93" fill="#6F6BF8" fillRule="nonzero" rx="5"/>
+        <text fill="#FFF" fontSize="14">
+          <tspan x="12" y="19">Do you have a specific </tspan> <tspan x="12" y="37">example of how your </tspan> <tspan x="12" y="55">component could be used </tspan> <tspan x="12" y="73">in a product?</tspan>
+        </text>
+      </g>
+    </g>
+  </svg>
+);
+
+export default Flowchart;
